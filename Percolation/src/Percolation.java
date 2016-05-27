856d1ea10ec3fe628a2609a9db7eb16f414cd428
Eric P <congpeng89@gmail.com>
2016-05-27 16:04:08 -0500

    Fixed backwash issue.

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -6,25 +6,29 @@ public class Percolation {
 	private int N;
 	private WeightedQuickUnionUF weightedUf;
 	private boolean[] sites;
+	private boolean percolates;
 	
 	//Virtual top: N*N
 	//Virtual bottom: N*N+1
+	
+	
 	
 	public Percolation(int N) {
 		//TODO: initialize N/X, all "blocked"
 		if(N > 0) {
 			this.N = N;
 			sites = new boolean[N*N];
-			weightedUf = new WeightedQuickUnionUF((N*N) + 2);
+			percolates = false;
+			weightedUf = new WeightedQuickUnionUF((N*N) + 1);
 			for(int i=0; i<sites.length; i++) {
 				sites[i] = false;
-				if(i >= 0 && i < N) {
-					//Virtual top
-					weightedUf.union(N*N, i);
-				} else if(i >= (N * (N-1)) && i < N*N) {
-					//Virtual bottom
-					weightedUf.union(N*N+1, i);
-				}
+//				if(i >= 0 && i < N) {
+//					//Virtual top
+//					weightedUf.union(N*N, i);
+//				} else if(i >= (N * (N-1)) && i < N*N) {
+//					//Virtual bottom
+//					weightedUf.union(N*N+1, i);
+//				}
 			}
 		} else {
 			throw new IllegalArgumentException("Input must be greater than 0");
@@ -41,23 +45,28 @@ public class Percolation {
 				if((i > 1) && (sites[xyToId(i-1, j)])) {
 					StdOut.print("1: ");
 					weightedUf.union(xyToId(i-1, j), xyToId(i, j));
-					printStuff();
+//					printStuff();
 				}
 				if((i < N) && (sites[xyToId(i+1, j)])) {
 					StdOut.print("2: ");
 					weightedUf.union(xyToId(i+1, j), xyToId(i, j));
-					printStuff();
+//					printStuff();
 				}
 				if((j > 1) && (sites[xyToId(i, j-1)])) {
 					StdOut.print("3: ");
 					weightedUf.union(xyToId(i, j), xyToId(i, j-1));
-					printStuff();
+//					printStuff();
 				}
 				if((j < N) && (sites[xyToId(i, j+1)])) {
 					StdOut.print("4: ");
 					weightedUf.union(xyToId(i, j), xyToId(i, j+1));
-					printStuff();
+//					printStuff();
 				}
+				
+				if(i == 1) {
+					weightedUf.union(xyToId(i, j), N*N);
+				}
+				
 				
 			}
 			
@@ -76,53 +85,69 @@ public class Percolation {
 	
 	public boolean isFull(int i, int j) {
 		if((i+j >= 2) && (i+j <= N*2)) {
-			if(isOpen(i, j))
-				return weightedUf.connected(xyToId(i, j), N*N);
+			if(weightedUf.connected(xyToId(i, j), N*N)) {
+				if(i < N)
+					return true;
+				else if(isOpen(i, j)) {
+					percolates = true;
+					return true;
+				}
+				else
+					return false;
+			}
 			else
 				return false;
-		}
+		} 
 		else {
 			throw new IndexOutOfBoundsException("Coordinates are out of range");
 		}
 	}
 	
 	public boolean percolates() {
-		return weightedUf.connected(N*N, N*N+1);
+		return percolates;
 	}
 	
 	private int xyToId(int row, int col) {
 		return ((row-1) * N) + col-1;
 	}
 	
-	public void printStuff() {
-		for(int i=0; i<sites.length; i++) {
-			System.out.print(weightedUf.find(i) + " ");
-		}
-		System.out.println("\n");
+	private int idtoX(int id) {
+		return id/N + 1;
 	}
 	
-	public void printSite(int i, int j) {
-		StdOut.println(xyToId(i, j) + "   " + sites[xyToId(i, j)]);
+	private int idtoY(int id, int row) {
+		return ((id - (row - 1))/N) + 1;
 	}
 	
-	public static void main(String[] args) {
-		Percolation perc = new Percolation(4);
-		StdOut.print("a: ");
-		perc.open(2, 3);
-		StdOut.print("b: ");
-		perc.open(3, 3);
-		StdOut.print("c: ");
-		perc.open(1, 3);
-		StdOut.print("d: ");
-		perc.open(4, 3);
-		StdOut.print("e: ");
-		perc.open(3, 2);
-		
-		
-		StdOut.println(perc.percolates());
-		StdOut.println(perc.isFull(3, 2));
-		
-		
-	}
+//	public void printStuff() {
+//		for(int i=0; i<sites.length; i++) {
+//			System.out.print(weightedUf.find(i) + " ");
+//		}
+//		System.out.println("\n");
+//	}
+//	
+//	public void printSite(int i, int j) {
+//		StdOut.println(xyToId(i, j) + "   " + sites[xyToId(i, j)]);
+//	}
+//	
+//	public static void main(String[] args) {
+//		Percolation perc = new Percolation(4);
+//		StdOut.print("a: ");
+//		perc.open(2, 3);
+//		StdOut.print("b: ");
+//		perc.open(3, 3);
+//		StdOut.print("c: ");
+//		perc.open(1, 3);
+//		StdOut.print("d: ");
+//		perc.open(4, 3);
+//		StdOut.print("e: ");
+//		perc.open(3, 2);
+//		
+//		
+//		StdOut.println(perc.percolates());
+//		StdOut.println(perc.isFull(3, 2));
+//		
+//		
+//	}
 	
 }
