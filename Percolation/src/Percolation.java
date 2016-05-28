3bc42e0d87ad469ed77d3a151ceba27dabf03ebf
Eric P <congpeng89@gmail.com>
2016-05-28 01:04:46 -0500

    Implemented PercolationStats
    Bug fixes for Percolation.java as well

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -1,4 +1,3 @@
-import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
@@ -8,29 +7,14 @@ public class Percolation {
 	private boolean[] sites;
 	private boolean percolates;
 	
-	//Virtual top: N*N
-	//Virtual bottom: N*N+1
-	
-	
-	
 	public Percolation(int N) {
-		//TODO: initialize N/X, all "blocked"
 		if(N > 0) {
 			this.N = N;
 			sites = new boolean[N*N];
 			percolates = false;
 			weightedUf = new WeightedQuickUnionUF((N*N) + 1);
-			for(int i=0; i<sites.length; i++) {
-				sites[i] = false;
-//				if(i >= 0 && i < N) {
-//					//Virtual top
-//					weightedUf.union(N*N, i);
-//				} else if(i >= (N * (N-1)) && i < N*N) {
-//					//Virtual bottom
-//					weightedUf.union(N*N+1, i);
-//				}
-			}
-		} else {
+		} 
+		else {
 			throw new IllegalArgumentException("Input must be greater than 0");
 		}
 	}
@@ -43,33 +27,23 @@ public class Percolation {
 				
 				
 				if((i > 1) && (sites[xyToId(i-1, j)])) {
-					StdOut.print("1: ");
-					weightedUf.union(xyToId(i-1, j), xyToId(i, j));
-//					printStuff();
+					weightedUf.union(xyToId(i, j), xyToId(i-1, j));
 				}
 				if((i < N) && (sites[xyToId(i+1, j)])) {
-					StdOut.print("2: ");
-					weightedUf.union(xyToId(i+1, j), xyToId(i, j));
-//					printStuff();
+					weightedUf.union(xyToId(i, j), xyToId(i+1, j));
 				}
 				if((j > 1) && (sites[xyToId(i, j-1)])) {
-					StdOut.print("3: ");
 					weightedUf.union(xyToId(i, j), xyToId(i, j-1));
-//					printStuff();
 				}
 				if((j < N) && (sites[xyToId(i, j+1)])) {
-					StdOut.print("4: ");
 					weightedUf.union(xyToId(i, j), xyToId(i, j+1));
-//					printStuff();
 				}
 				
 				if(i == 1) {
 					weightedUf.union(xyToId(i, j), N*N);
 				}
 				
-				
 			}
-			
 		}
 		else
 			throw new IndexOutOfBoundsException("Coordinates are out of range");
@@ -111,24 +85,6 @@ public class Percolation {
 		return ((row-1) * N) + col-1;
 	}
 	
-	private int idtoX(int id) {
-		return id/N + 1;
-	}
-	
-	private int idtoY(int id, int row) {
-		return ((id - (row - 1))/N) + 1;
-	}
-	
-//	public void printStuff() {
-//		for(int i=0; i<sites.length; i++) {
-//			System.out.print(weightedUf.find(i) + " ");
-//		}
-//		System.out.println("\n");
-//	}
-//	
-//	public void printSite(int i, int j) {
-//		StdOut.println(xyToId(i, j) + "   " + sites[xyToId(i, j)]);
-//	}
 //	
 //	public static void main(String[] args) {
 //		Percolation perc = new Percolation(4);
--- Percolation/src/PercolationStats.java
+++ Percolation/src/PercolationStats.java
@@ -1,27 +1,56 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 	
+	private int iterations;
+	private Percolation perc;
+	private double[] fracs;
+	
 	public PercolationStats(int N, int T) {
 		if(N > 0 && T > 0) {
-			
+			iterations = T;	
+			fracs = new double[N];
+			for(int counter=0; counter<iterations; counter++) {
+				perc = new Percolation(N);
+				int open = 0;
+				while(!perc.percolates()) {
+					int i = StdRandom.uniform(1, N + 1);
+	                int j = StdRandom.uniform(1, N + 1);
+	                if (!perc.isOpen(i, j)) {
+	                    perc.open(i, j);
+	                	perc.isFull(i, j);
+	                    open++;
+	                }
+				}
+				
+				double fraction = (double) open / (N * N);
+	            fracs[counter] = fraction;
+			}
 		} else {
 			throw new IllegalArgumentException("Inputted values must be greater than 0");
 		}
 	}
 	
 	public double mean() {
-		return 0.0;
+		return StdStats.mean(fracs);	
 	}
 	
 	public double stddev() {
-		return 0.0;
+		return StdStats.stddev(fracs);
 	}
 	
 	public double confidenceLo() {
-		return 0.0;
+		return mean() - (1.96 * (stddev()/Math.sqrt(iterations)));
 	}
 	
 	public double confidenceHi() {
-		return 0.0;
+		return mean() + (1.96 * (stddev()/Math.sqrt(iterations)));
+	}
+	
+	public static void main(String...args) {
+		PercolationStats percStats = new PercolationStats(200, 100);
+		StdOut.println(percStats.mean() + "\n" + percStats.stddev() + "\n" + percStats.confidenceLo() + "\n" + percStats.confidenceHi());
 	}
 }
