b5dd73fecba2633e52bf5a1265af71670f2c48f0
Eric P <congpeng89@gmail.com>
2016-08-09 12:50:13 -0500

    Added generics implementation to the Stack class.
    There is an ugly cast...but meh.

--- ScientificOp/src/Stack.java
+++ ScientificOp/src/Stack.java
@@ -1,41 +1,42 @@
 
 
-public class Stack {
+public class Stack<Item> {
 	
 	/*
 	 * I know I could have used a linked-list type of implementation. I just wanted to use an Array implementation.
 	 */
 	
 	private int position;
-	private String[] theStack;
+	private Item[] theStack;
 	
 	public Stack() {
-		position = -1;
-		theStack = new String[5];
+		position = 0;
+		theStack = (Item[]) new Object[5];
 	}
 	
-	public void push(String token) {
+	public void push(Item token) {
 		if(position == theStack.length)
 			resizeArray(theStack.length*2);
 		theStack[position++] = token;
+		
 	}
 	
-	public String pop() {
+	public Item pop() {
 		if(position >= 0)
 			return theStack[--position];
 		else
 			return null;
 	}
 	
-	public String peek() {
+	public Item peek() {
 		if(position >= 0)
-			return theStack[position];
+			return theStack[position-1];
 		else
 			return null;
 	}
 	
 	private void resizeArray(int cap) {
-		String[] temp = new String[cap];
+		Item[] temp = (Item[])new Object[cap];
 		for(int i=0; i<position; i++) {
 			temp[i] = theStack[i];
 		}
