6d6eff7d6c4ac9ed2c06025c4a76c0261245afdc
Eric P <congpeng89@gmail.com>
2016-08-11 16:00:32 -0500

    Modified Stack.java
    Added methods, such as peekAt, pushAt, etc. I know, know this isn't
    really a stack data structure.

--- ScientificOp/src/Stack.java
+++ ScientificOp/src/Stack.java
@@ -6,38 +6,91 @@ public class Stack<Item> {
 	 * I know I could have used a linked-list type of implementation. I just wanted to use an Array implementation.
 	 */
 	
-	private int position;
+	private int topOfStack;
 	private Item[] theStack;
 	
+	@SuppressWarnings("unchecked")
 	public Stack() {
-		position = 0;
+		topOfStack = 0;
 		theStack = (Item[]) new Object[5];
 	}
 	
 	public void push(Item token) {
-		if(position == theStack.length)
+		if(topOfStack == theStack.length)
 			resizeArray(theStack.length*2);
-		theStack[position++] = token;
+		theStack[topOfStack++] = token;
 		
 	}
 	
 	public Item pop() {
-		if(position >= 0)
-			return theStack[--position];
+		if(topOfStack >= 0) {
+			Item obj = theStack[--topOfStack];
+			theStack[topOfStack] = null;
+			if(topOfStack > 0 && topOfStack == theStack.length/4)
+				resizeArray(theStack.length/2);
+			return obj;
+		}
 		else
 			return null;
 	}
 	
 	public Item peek() {
-		if(position >= 0)
-			return theStack[position-1];
+		if(topOfStack >= 0)
+			return theStack[topOfStack];
 		else
 			return null;
 	}
 	
+	public int getStackLength() {
+		return theStack.length;
+	}
+	
+	public int getTopOfStackPos() {
+		return topOfStack;
+	}
+	
+	public boolean isEmpty() {
+		return topOfStack == 0;
+	}
+	
+	public Item peekAt(int position) {
+		if(position < 0 || position >= getTopOfStackPos())
+				return null;
+		return theStack[position];
+	}
+	
+	public Item popAt(int position) {
+		if(position > this.topOfStack || position < 0) {
+			System.err.println("Index out of bounds");
+			return null;
+		}
+		Item item = theStack[position];
+		shiftDown(position);
+		return item;
+		
+	}
+	
+	public void pushAt(Item obj, int position) {
+		if(this.topOfStack == theStack.length)
+			resizeArray(theStack.length*2);
+		for(int i=getTopOfStackPos(); i>=position; i--) {
+			theStack[i+1] = theStack[i];
+		}
+		theStack[position] = obj;
+		this.topOfStack++;
+	}
+	
+	private void shiftDown(int pos) {
+		for(int i=pos; i<getTopOfStackPos()-1; i++) {
+			theStack[i] = theStack[i+1];
+		}
+		topOfStack--;
+	}
+
+	@SuppressWarnings("unchecked")
 	private void resizeArray(int cap) {
 		Item[] temp = (Item[])new Object[cap];
-		for(int i=0; i<position; i++) {
+		for(int i=0; i<topOfStack; i++) {
 			temp[i] = theStack[i];
 		}
 		theStack = temp;
